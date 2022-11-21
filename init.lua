dc4901ab9dde5b58c5b60f6332bfb3d11de45f4d
Leo Aparisi de Lannoy <laparisidelannoy@uchicago.edu>
2022-11-21 00:02:28 -0600

    highlight

--- init.lua
+++ init.lua
@@ -7,7 +7,9 @@ vim.opt.breakindent = true
 vim.opt.tabstop = 4
 vim.opt.shiftwidth = 4
 vim.opt.termguicolors = true
-  vim.o.autochdir          = true
+vim.opt.autochdir          = true
+vim.opt.cursorline= true
+vim.opt.cursorlineopt= "number"
 require('configs')                  -- plugin independent configs
 require('mappings')                 -- plugin independent mappings
 require('packer_nvim')
