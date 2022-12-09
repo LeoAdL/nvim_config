db91e70c4668b387dd4340d2138cd05c93dcdbe0
Leo Aparisi de Lannoy <laparisidelannoy@uchicago.edu>
2022-12-08 18:01:28 -0600

    Fix Nulls_ls

--- init.lua
+++ init.lua
@@ -1,29 +1,28 @@
 if vim.g.vscode then
-    -- VSCode extension
-require('leap').add_default_mappings()
+	-- VSCode extension
+	require("leap").add_default_mappings()
 else
-vim.opt.number = true
-vim.opt.mouse = 'a'
-vim.opt.ignorecase = true
-vim.opt.smartcase = true
-vim.opt.wrap = true
-vim.opt.breakindent = true
-vim.opt.tabstop = 4
-vim.opt.shiftwidth = 4
-vim.opt.termguicolors = true
-vim.opt.autochdir          = true
-vim.opt.cursorline= true
-vim.opt.cursorlineopt= "number"
-vim.opt.relativenumber = true
-vim.opt.signcolumn = "yes"           
-vim.opt.smartindent= true 
-vim.g.vimtex_compiler_progname='nvr'
-    -- ordinary Neovim
+	vim.opt.number = true
+	vim.opt.mouse = "a"
+	vim.opt.ignorecase = true
+	vim.opt.smartcase = true
+	vim.opt.wrap = true
+	vim.opt.breakindent = true
+	vim.opt.tabstop = 4
+	vim.opt.shiftwidth = 4
+	vim.opt.termguicolors = true
+	vim.opt.autochdir = true
+	vim.opt.cursorline = true
+	vim.opt.cursorlineopt = "number"
+	vim.opt.relativenumber = true
+	vim.opt.signcolumn = "yes"
+	vim.opt.smartindent = true
+	vim.g.vimtex_compiler_progname = "nvr"
+	-- ordinary Neovim
 
-
-require('impatient')
-require('configs')                  -- plugin independent configs
-require('mappings')                 -- plugin independent mappings
-require('packer_nvim')
-vim.g.tex_flavor = 'latex'
+	require("impatient")
+	require("configs") -- plugin independent configs
+	require("mappings") -- plugin independent mappings
+	require("packer_nvim")
+	vim.g.tex_flavor = "latex"
 end
--- lua/configs/null_ls_vim.lua
+++ lua/configs/null_ls_vim.lua
@@ -1,27 +1,12 @@
 local null_ls = require("null-ls")
-null_ls.setup({
-    sources = {
-		null_ls.builtins.formatting.black,
-		null_ls.builtins.formatting.latexindent,
-		null_ls.builtins.diagnostics.chktex,
-		null_ls.builtins.diagnostics.flake8,
-		null_ls.builtins.formatting.prettier,
-        null_ls.builtins.formatting.reorder_python_imports,
-        null_ls.builtins.diagnostics.pycodestyle,
-        null_ls.builtins.diagnostics.pydocstyle,
-        null_ls.builtins.completion.luasnip,
-        null_ls.builtins.diagnostics.luacheck,
-		null_ls.builtins.formatting.stylua,
-    }}
-)
 local lsp_formatting = function(bufnr)
-    vim.lsp.buf.format({
-        filter = function(client)
-            -- apply whatever logic you want (in this example, we'll only use null-ls)
-            return client.name == "null-ls"
-        end,
-        bufnr = bufnr,
-    })
+	vim.lsp.buf.format({
+		filter = function(client)
+			-- apply whatever logic you want (in this example, we'll only use null-ls)
+			return client.name == "null-ls"
+		end,
+		bufnr = bufnr,
+	})
 end
 
 -- if you want to set up formatting on save, you can use this as a callback
@@ -29,14 +14,30 @@ local augroup = vim.api.nvim_create_augroup("LspFormatting", {})
 
 -- add to your shared on_attach callback
 local on_attach = function(client, bufnr)
-    if client.supports_method("textDocument/formatting") then
-        vim.api.nvim_clear_autocmds({ group = augroup, buffer = bufnr })
-        vim.api.nvim_create_autocmd("BufWritePre", {
-            group = augroup,
-            buffer = bufnr,
-            callback = function()
-                lsp_formatting(bufnr)
-            end,
-        })
-    end
+	if client.supports_method("textDocument/formatting") then
+		vim.api.nvim_clear_autocmds({ group = augroup, buffer = bufnr })
+		vim.api.nvim_create_autocmd("BufWritePre", {
+			group = augroup,
+			buffer = bufnr,
+			callback = function()
+				lsp_formatting(bufnr)
+			end,
+		})
+	end
 end
+
+null_ls.setup({
+	on_attach = on_attach,
+	sources = {
+		null_ls.builtins.formatting.black,
+		null_ls.builtins.formatting.latexindent,
+		null_ls.builtins.diagnostics.chktex,
+		null_ls.builtins.diagnostics.flake8,
+		null_ls.builtins.formatting.prettier,
+		null_ls.builtins.formatting.reorder_python_imports,
+		null_ls.builtins.diagnostics.pylint,
+		null_ls.builtins.completion.luasnip,
+		null_ls.builtins.diagnostics.luacheck,
+		null_ls.builtins.formatting.stylua,
+	},
+})
